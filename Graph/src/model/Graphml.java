f9ec77e48bb553ef7f32e65d37ed1ad13707d6c9
AnaCarolyne <anacarolyne.franca@gmail.com>
2017-09-27 16:33:26 -0300

    Criei a classe Graphml.

--- Graph/src/model/Graphml.java
+++ Graph/src/model/Graphml.java
@@ -4,11 +4,26 @@
  * and open the template in the editor.
  */
 package model;
-
 /**
  *
- * @author Anah
+ * @author Ana Carolyne
  */
 public class Graphml {
-    
+    private Graph graph;
+    private String xmlns = "http://graphml.graphdrawing.org/xmlns";
+
+    public Graph getGraph() {
+        return graph;
+    }
+
+    public void setGraph(Graph graph) {
+        this.graph = graph;
+    }
+
+    public String getXmlns() {
+        return xmlns;
+    }
+    public void setXmlns(String xmlns) {
+        this.xmlns = xmlns;
+    }
 }
