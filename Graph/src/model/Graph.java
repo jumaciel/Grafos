919e70954d412ab52f563ecf9877409094a07193
AnaCarolyne <anacarolyne.franca@gmail.com>
2017-10-25 14:06:37 -0200

    Adcionando Grau e Matriz Adjacencia-Ana Carolyne

--- Graph/src/model/Graph.java
+++ Graph/src/model/Graph.java
@@ -91,5 +91,39 @@ public class Graph {
         addEdge(e);
 
     }
+     public String getGrau(Graph graph) {
+       String listaGrau = "";
+      
+        for (Edge e : graph.getEdges()) {  //Varre a lista de aresta contando quantas vezes o vertice aparece
+            e.getNode1().setGrau(e.getNode1().getGrau() + 1);
+            e.getNode2().setGrau(e.getNode2().getGrau() + 1);
+        }
+        for (Node no : graph.getNodes()) {
+            listaGrau += "\n Grau Vertice " + no.getId() + " é " + no.getGrau();
+        }         
+        return listaGrau;
+    }
+    public String matrizAdjacencia(Graph graph) {
+        String espaco = "    ";
+        int size = graph.getNodes().size(), i, j;
+        int matriz[][] = new int[size][size];
+        String matrizTotal = "  ";
+        for (Edge e : graph.getEdges()) {
+            int no1 = graph.getNodes().indexOf(e.getNode1());
+            int no2 = graph.getNodes().indexOf(e.getNode2());
+            matriz[no1][no2] = 1;
+            matriz[no2][no1] = 1;
+        }
+        for (int a = 0; a < size; a++) {
+            matrizTotal += espaco + graph.getNodes().get(a).getId();
+        }
+        for (i = 0; i < size; i++) {
+            matrizTotal += "\n" + graph.getNodes().get(i).getId();
+            for (j = 0; j < size; j++) {
+                matrizTotal += espaco + matriz[i][j];
+            }
+        }
+        return (matrizTotal);
+    }
 
 }
